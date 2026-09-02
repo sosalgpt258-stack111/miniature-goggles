465d3916464ff47e6a7127a7c460eae5c6ebf4aa
sosalgpt258-stack111 <sosalgpt258@gmail.com>
2026-09-02 18:32:55 +0700

    main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,29 @@
+-- Astra Main Script (main.lua)
 
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/sosalgpt258-stack111/miniature-goggles/main/lib.lua"))()
+local ESPModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/sosalgpt258-stack111/miniature-goggles/main/esp.lua"))()
+
+-- Инициализация окна
+local Window = Library.New()
+
+-- Создание вкладок
+local AimbotTab = Window:CreateTab("Aimbot", 1)
+local VisualsTab = Window:CreateTab("Visuals", 2)
+local MiscTab = Window:CreateTab("Misc", 3)
+local SettingsTab = Window:CreateTab("Settings", 4)
+local InfoTab = Window:CreateTab("Info", 5)
+
+-- Колонка визуалов
+local visualsCol = VisualsTab:CreateColumn("esp settings", UDim2.new(0, 12, 0, 10), UDim2.new(0, 195, 0, 355))
+local esp = ESPModule.Init()
+
+visualsCol:CreateToggle("box corners", function(state)
+    esp.SetEnabled(state)
+end)
+
+-- Колонка настроек
+local settingsCol = SettingsTab:CreateColumn("management", UDim2.new(0, 12, 0, 10), UDim2.new(0, 195, 0, 355))
+
+settingsCol:CreateButton("unload script", function()
+    Window:Unload()
+end)
